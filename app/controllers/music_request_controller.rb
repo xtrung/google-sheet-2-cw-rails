ca19d7a6503c3aea1acceb3c4ed5e11cc1c1856c
Trungx <xtrung.net@gmail.com>
2017-03-14 13:46:34 +0700

    Implement edit request feature

--- app/controllers/music_request_controller.rb
+++ app/controllers/music_request_controller.rb
@@ -11,15 +11,42 @@ class MusicRequestController < ApplicationController
 		approve_row(approve_index)
 		redirect_back fallback_location: {action: "index"}
 	end
+
+	def edit
+		row_timespan = params[:id]
+		@music = get_music_data.select { |item| item[0] == row_timespan }.first
+	end
+
+	def update
+		row_timespan = params[:id]
+		@music_data ||= get_music_data
+		approve_index = @music_data.index{ |item| item[0] == row_timespan }
+		update_row(approve_index, params)
+		redirect_back fallback_location: {action: "index"}	
+	end
+
+
 	def get_music_data
 		Rails.cache.fetch('music_data:', exprise_in: 12.seconds) {
 			gg_service = GoogleSheetService.new
 			gg_service.read
 		}
 	end
+	
 	def approve_row(row)
 		gg_service = GoogleSheetService.new
-		gg_service.write(row + 2, 4, 1)
+		gg_service.write(row + 1, 4, 1)
 		@music_data[row][3] = 1
 	end
+
+	def update_row(row, data)
+		gg_service = GoogleSheetService.new
+		gg_service.write(row + 1, 2, data[:name])
+		gg_service.write(row + 1, 3, data[:content])
+		gg_service.write(row + 1, 4, data[:is_approve])
+		@music_data[row][1] = data[:name]
+		@music_data[row][2] = data[:content]
+		@music_data[row][3] = data[:is_approve]
+	end
+
 end
